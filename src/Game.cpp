9e574cafa9469e615390e0b7699d00649c484d73
Aslam-Naseer <aslamnaseer22@gmail.com>
2025-07-16 20:17:47 +0530

    Update to fullscreen

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 
 void Game::initWindow() {
-    window = sf::RenderWindow(sf::VideoMode({ windowWidth, windowHeight }), windowTitle);
+    window = sf::RenderWindow(sf::VideoMode().getDesktopMode(), windowTitle, sf::Style::Default, sf::State::Fullscreen);
     window.setFramerateLimit(144);
 }
 
@@ -32,9 +32,11 @@ void Game::render() {
 }
 
 Game::Game(const std::string& title, int width, int height) :
-    windowWidth(width), windowHeight(height), windowTitle(title), player("assets/Player", 0.05, 500.f)
+    windowTitle(title), player("assets/Player", 0.05, 500.f)
 {
     initWindow();
+	windowWidth = window.getSize().x;
+	windowHeight = window.getSize().y;
     player.setPosition(windowWidth / 2.f, windowHeight / 2.f);
 }
 
